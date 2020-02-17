b642e8f51aa75e46d47ac8e76c05ed3d776b6c3d
Katie88 <kathrynhoth@gmail.com>
2020-02-16 17:34:27 -0700

    adding server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const PORT = process.env.PORT || 3000
+const app = express();
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI) || "mongodb:localhost/userdb",{
+    useNewUrlParser:true,
+    useFindAndModify:false
+});
+
+app.use(require("./routes/html-routes.js"));
+app.use(require("./routes/api-routes.js"));
+app.listen(PORT,()=>{
+    console.log('App running on Port: ${PORT}');
+});
